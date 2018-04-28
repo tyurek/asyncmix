aa72f7b136e4a56fb20bb575ab3c9b8ccec10f86
Samarth Kulshreshtha <smkuls@gmail.com>
2018-04-27 19:05:06 -0500

    Changing to TCP

--- HelperClasses.py
+++ HelperClasses.py
@@ -49,7 +49,7 @@ class Listener(object):
         # To reuse the same address again since this is a daemon thread.
         self.listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.listener.bind(('', listener_port))
-        self.MAX_BYTES = 1024*32
+        self.MAX_BYTES = 1024*1024
         thread = Thread(target=self.__start_listener)
         thread.setDaemon(True)
         self.queue = Queue()
